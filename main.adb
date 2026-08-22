3fde27d6e0488a2c9d05379a8a46a9325435938f
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-22 18:43:28 +0200

    Implement Levenshtein Coding Core algorithm

---
+++ main.adb
@@ -0,0 +1,7 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Main is
+begin
+   Put_Line ("Levenshtein Coding Core algorithm implemented.");
+   Put_Line ("Please run 'make test' to execute the Validation (V&V) suite.");
+end Main;
